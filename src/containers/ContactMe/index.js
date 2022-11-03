b6b8f56c922aad10df19778e9a9a5ea50fc0be5f
Kennybabs-py <babalolakehinde74@gmail.com>
2022-11-03 15:48:08 +0100

    feat: added input components

--- src/containers/ContactMe/index.js
+++ src/containers/ContactMe/index.js
@@ -1,3 +1,4 @@
+import Input from "components/Input";
 import "./styles.scss";
 
 function ContactMe() {
@@ -7,6 +8,32 @@ function ContactMe() {
         <h2>Contact Me</h2>
 
         <p>Hi there, contact me to ask me about anything you have in mind.</p>
+      </section>
+
+      <section className="form__container">
+        <form action="">
+          <div className="names__box">
+            <div>
+              <Input
+                type="text"
+                placeholder="Enter your first name"
+                id="first_name"
+              />
+            </div>
+
+            <div>
+              <Input
+                type="text"
+                placeholder="Enter your last name"
+                id="last_name"
+              />
+            </div>
+          </div>
+
+          <div className="email__box"></div>
+
+          <div className="message__box"></div>
+        </form>
       </section>
     </main>
   );
