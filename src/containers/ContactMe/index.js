27980a2ce4ee83ab72e76c56badb487a72863512
Kennybabs-py <babalolakehinde74@gmail.com>
2022-11-03 15:20:51 +0100

    feat: update to contact page

--- src/containers/ContactMe/index.js
+++ src/containers/ContactMe/index.js
@@ -4,10 +4,9 @@ function ContactMe() {
   return (
     <main className="contact__me">
       <section className="header">
-        <h2>
-          Contact Me
-          <p>Hi there, contact me to ask me about anything you have in mind.</p>
-        </h2>
+        <h2>Contact Me</h2>
+
+        <p>Hi there, contact me to ask me about anything you have in mind.</p>
       </section>
     </main>
   );
